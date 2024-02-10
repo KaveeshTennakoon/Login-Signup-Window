a5398f025cfe788476deb0a795bc813b320e742b
Kaveesh Tennakoon <kaveeshoffical@gmial.com>
2024-02-10 19:45:30 +0530

    update of java class for Login

--- src/main/java/com/app/useracc/Login.java
+++ src/main/java/com/app/useracc/Login.java
@@ -1,4 +1,27 @@
 package com.app.useracc;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
 public class Login {
+
+    @FXML
+    private Label ErrorMsg;
+
+    @FXML
+    private Button LoginButton;
+
+    @FXML
+    private BorderPane LoginWin;
+
+    @FXML
+    private PasswordField Password;
+
+    @FXML
+    private TextField Username;
+
 }
